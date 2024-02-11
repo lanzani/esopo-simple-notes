0cf80649b85c41eadea800d23e0a0179dc941e01
Federico Lanzani <federico@federicolanzani.com>
2024-02-11 01:16:24 +0100

    init

---
+++ src/preload/index.ts
@@ -0,0 +1,14 @@
+import { contextBridge } from 'electron'
+import { electronAPI } from '@electron-toolkit/preload'
+
+if (!process.contextIsolated) {
+  throw new Error('contextIsolation must be enabled in the BrowserWindow')
+}
+
+try {
+  contextBridge.exposeInMainWorld('context', {
+    //TODO
+  })
+} catch (error) {
+  console.log(error)
+}
