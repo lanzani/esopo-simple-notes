72209e96e559f0fc0b21ef48199d40ad1a86811e
Federico Lanzani <federico@federicolanzani.com>
2024-02-13 13:32:52 +0100

    :rocket: Delete note and welcome note.

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,5 +1,5 @@
 export const appDirectoryName = 'esopo-simple-notes'
 export const fileEncoding = 'utf8'
 
-export const autoSaveTime = 3000
+export const autoSaveTime = 1000
 export const welcomeNoteFilename = 'Welcome'
